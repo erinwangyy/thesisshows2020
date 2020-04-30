86727fde6b14a7fa6674499c4aa36995bd905c87
erinwangyy <59551284+erinwangyy@users.noreply.github.com>
2020-04-29 23:19:28 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function ccamenu(){
+
+document.querySelector(".ccaoption").style.display="block"
+console.log("yes")
+}
+
+function ccamenu2(){
+    document.querySelector(".ccaoption").style.display="none"
+}
